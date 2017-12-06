f9b9159a04e3afbd5a335880e072c0b241c67db2
404harambe <v.pandelea@studenti.unipi.it>
2017-12-06 18:41:07 +0100

    bp

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from sklearn import preprocessing
 
 train_data_path = "ML-CUP17-TR.csv"
 test_data_path = "ML-CUP17-TS.csv"
-np.random.seed(4)
 
 class Dataset:
     def __init__(self):
@@ -94,7 +93,17 @@ dataset.init_test(load_data(test_data_path, False, header_l=10))
 preprocessor = Preprocessor()
 
 def sigmoid(x):
+    #print(x)
     return 1/(1+np.exp(-x))
+
+def sigmoddxf(x):
+    return x*(1-x)
+
+def linear(x):
+    return x
+
+def lineardxf(x):
+    return 1
 
 class Activation:
 
@@ -104,10 +113,20 @@ class Activation:
 
 class SimpleOptimizer:
 
-    def optimize(self, y_true, epochs, NN, maxIters):
-        for i in range(epochs):
-            o = NN.fp
-            NN.bp(o, y_true)
+    def __init__(self,lr):
+        self.lr = lr
+    def optimize(self, f, W):
+        #Could use weights instead of NN
+        loss, grad = f(W)
+        print(loss)
+
+        return -self.lr*grad
+
+       # for i in range(epochs):
+        #    o = NN.fp(x_in)
+         #   loss, grad = NN.bp(o, y_true, x_in)
+          #  print("Loss:"+str(loss))
+            #TODO other constraints
             #update weight
             #for layer in NN.layers:
                 #layer.weights = layer.weights+layer.gradients
@@ -115,12 +134,13 @@ class SimpleOptimizer:
 #preprocessing
 class Layer:
 
-    def __init__(self, inputs, neurons, activation, weights=None, bias=0):
+    def __init__(self, inputs, neurons, activation, weights=np.array(None), bias=0):
         self.currentOutput = None
+        self.grad=None
         if inputs<0 or neurons<0: sys.exit("Expected positive value")
         if weights.any()==None:
             self.W = np.random.rand(neurons, inputs+1)
-            self.W = np.ones((neurons, inputs+1))
+            #self.W = np.ones((neurons, inputs+1))
         else:
             if isinstance(weights, (np.ndarray, np.generic)):
                 if (weights.shape!=(neurons, inputs)):
@@ -131,13 +151,11 @@ class Layer:
                     self.W[:, 0] = bias
             else:
                 sys.exit("Expected a nparray")
-
+        print(self.W)
         self.activation = activation #check for errors(wheter its a func or not)
 
     def getOutput(self,x):
         x = np.concatenate((np.ones((x.shape[0],1)),x),axis=1)
-        print(x)
-        print(self.W.transpose())
         partial = np.dot(x, self.W.transpose())
         self.currentOutput = self.activation.f(partial)
         return self.currentOutput
@@ -149,7 +167,7 @@ class NeuralNetwork:
         #TODO initial layers
         self.layers = []
 
-    def addLayer(self,inputs, neurons, activation, weights=None, bias=0):
+    def addLayer(self,inputs, neurons, activation, weights=np.array(None), bias=0):
         self.layers.append(Layer(inputs, neurons, activation, weights, bias=bias))
 
     def FP(self, x_in):
@@ -159,38 +177,91 @@ class NeuralNetwork:
         return x
 
     def BP(self, prediction, real, x_in):
+        gradients = []
+        loss_func = np.sum(1/(2*prediction.shape[0]) * ((real - prediction) * \
+                    (real - prediction)))
         for i in range(len(self.layers)-1, -1, -1):
-            loss_func = 1 / 2 * (real - self.layers[i].currentOutput) * \
-                        (real - self.layers[i].currentOutput)
-            der_loss_func = (prediction - real)
-            logi = self.layers[i].currentOutput * (1 - self.layers[i].currentOutput)
-            if i==(len(self.layers)-1):err = logi*der_loss_func
+
+            logi = self.layers[i].activation.dxf(self.layers[i].currentOutput)
+
+            #logi = self.layers[i].currentOutput * (1 - self.layers[i].currentOutput)
+
+            if i==(len(self.layers)-1):
+                err = logi*(prediction - real)
             else:
-                err=np.dot(err,self.layers[i+1].W[:,1:])
-            if i==0:curro = x_in
+                err=np.dot(err,self.layers[i+1].W[:,1:])*logi #error is derivative of activation
+                #at current layer * (weights*error at next layer)
+            if i==0:
+                curro = x_in
             else:
                 curro = self.layers[i-1].currentOutput
-            grad = curro*err*logi #TODO save gradient in layer
+            curro = np.concatenate((np.ones((curro.shape[0], 1)), curro), axis=1)
+            grad = np.dot(curro.transpose(),err) #TODO save gradient in layer
+            self.layers[i].grad = grad
+            gradients.append(grad)
+        return loss_func, np.array(gradients)
 
 
-    #def fit():
+    def f(self, chunk):
+        def g(W):
+            return self.BP(self.FP(chunk), self.real, chunk)
+        return g
+
+    def fit(self, dataset, epochs, batch_size=len(dataset.train[0])):
+        self.real = dataset.train[1]
+        for i in range(0, epochs):
+            for chunk in range(0,len(dataset.train[0]),batch_size):
+                cap = min([len(dataset.train[0]), chunk + batch_size])
+                update = optimizer.optimize(self.f(dataset.train[0][chunk:cap]), "ciao")
+                for i in range (0,len(self.layers)):
+                    self.layers[i].W = self.layers[i].W+update[-i-1].transpose()
+
+
+
+
         #gradient calculation is the same for all optimizers,
         #the update rule changes for each one
         #while backprop set layer.gradient
      #   self.optimizer.optimize(self.loss_func(dataset.train))
 
 
+toyx = np.random.rand(1016,11) #TODO make it work with arrays
+activation = Activation(sigmoid,sigmoddxf,sigmoid)
+activation1 = Activation(linear,lineardxf,sigmoid)
+NN = NeuralNetwork()
+NN.addLayer(11,2,activation)
+optimizer = SimpleOptimizer(0.01)
+#dataset.train[0] = toyx
+#dataset.train[1] = np.random.rand(1,2)
+preprocessor.normalize(dataset)
+#NN.fit(dataset, 3000, len(dataset.train[0]))
+
 toyx = np.asarray([[0.05,0.1]]) #TODO make it work with arrays
-activation = Activation(sigmoid,sigmoid,sigmoid)
+activation = Activation(sigmoid,sigmoddxf,sigmoid)
 NN = NeuralNetwork()
 NN.addLayer(2,2,activation,weights=np.array([[0.15,0.20],[0.25,0.3]]),bias=0.35)
 NN.addLayer(2,2,activation,weights=np.array([[0.40,0.45],[0.50,0.55]]),bias=0.6)
-o = NN.FP(toyx)
-print(o)
+dataset.train[0] = toyx
+dataset.train[1] = np.array([0.01, 0.99])
+
+#NN.fit(dataset, 1111, len(dataset.train[0]))
+optimizer = SimpleOptimizer(lr=1.72)
+
+dataset.train[0] = np.array([[3,4]])
+dataset.train[1] = np.array([5])
+NN = NeuralNetwork()
+NN.addLayer(2,1,activation)
+NN.fit(dataset, 50000, len(dataset.train[0]))
+for l in NN.layers:
+    print(l.W)
+'''o = NN.FP(toyx)
 print("==========================")
 o1 = NN.BP(o,np.array([0.01,0.99]),toyx)
+for l in NN.layers:
+    l.W = l.W - 0.5*l.grad.transpose()
+    print(l.W)
 print(o1)
-'''NN = NeuralNetwork()
+NN = NeuralNetwork()
 NN.addLayer(type,regularizer,neurons,activation..)
 
 NN.addLayers(type,[array of neurons], <number of layers>,
