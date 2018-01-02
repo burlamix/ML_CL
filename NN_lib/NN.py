Fixed regularization(sum to gradient before handing it over to the optimizer). Added elastic net.

NN_lib/NN.py
@@ -12,7 +12,7 @@ class NeuralNetwork:
         self.layers = []
 
     def addLayer(self, inputs, neurons, activation, weights=np.array(None), bias=0,
-                 regularization="L2", rlambda=0.0, weights_init='fan_in', dropout=0.5):
+                 regularization="L2", rlambda=0.0, weights_init='fan_in', dropout=0.0):
         self.layers.append(Layer(inputs, neurons, activation, weights, bias, dropout=dropout,
                                  regularizer=regularization, rlambda=rlambda, weights_init=weights_init))
 
@@ -57,8 +57,10 @@ class NeuralNetwork:
             # print('err', curro)
 
             # print('curr',curro.shape)
-            grad = np.dot(curro.transpose(), err) / (real.shape[0])
-            # print(grad)
+            grad = (np.dot(curro.transpose(), err) / (real.shape[0]))
+            #print(self.reguldx(i).shape)
+
+            grad= grad + self.reguldx(i).transpose()#/real.shape[0]
 
             # grad = (np.dot(curro.transpose(),err))
             self.layers[i].grad = grad
@@ -94,7 +96,10 @@ class NeuralNetwork:
                 return self.loss_func.f(out_chunk, self.FP(in_chunk))  # + self.regul()
             else:
                 loss, grad = self.BP(self.FP(in_chunk), out_chunk, in_chunk)
-                return loss + self.regul(), grad
+                #print('grad',grad.shape)
+               # print('after',(grad+np.array([self.reguldx(i) for i in range(0,len(self.layers))])
+#).shap#e)
+                return loss + self.regul(), grad#+np.array([self.reguldx(i) for i in range(0,len(self.layers))])
 
         return g
 
@@ -126,9 +131,8 @@ class NeuralNetwork:
             validation_y = val_set[1]
 
         history = {'tr_loss': [], 'val_loss': [], 'tr_acc': [], 'val_acc': []}
-
+        optimizer.reset()
         for i in range(0, epochs):
-
             # Randomly permute the data before each epoch
             perm = np.random.permutation(len(x_in))
             x_in = x_in[perm]
@@ -139,13 +143,13 @@ class NeuralNetwork:
                 cap = min([len(x_in), chunk + batch_size])
 
                 update = optimizer.optimize(self.f(x_in[chunk:cap], y_out[chunk:cap]), self.get_weight())
-
+                print('s',update.shape)
                 # predicted = self.FP(x_in[chunk:cap],)
                 for j in range(0, len(self.layers)):
                     # print("-----1----",update[j])
                     # print("-----2----",(self.reguldx(j) / batch_size).transpose())
-                    update[j] -= (self.reguldx(j) / 1).transpose()
-                    update[j] /= 1
+                    update[j] = update[j]#-optimizer.lr* (self.reguldx(j)).transpose()
+                    #update[j] /= 1
 
                 self.set_weight(update)
 
@@ -189,7 +193,7 @@ class NeuralNetwork:
         real = self.FP(x_in)
 
         # val_loss_func = self.loss_func[0](real,dataset.test[0]) #+ self.regul()        TODO TODO TODO MUST cambiare così
-        val_loss_func = loss_fun.f(real, y_out)  # + self.regul()
+        val_loss_func = loss_fun.f(real, y_out)   + self.regul()
 
         correct = 0
         errate = 0

NN_lib/layer.py
@@ -6,7 +6,7 @@ import sys
 class Layer:
 
     def __init__(self, inputs, neurons, activation, weights=np.array(None), bias=0, weights_init='fan_in',
-                 regularizer="L2", rlambda = 0.00, dropout=0.5):
+                 regularizer="L2", rlambda = 0.00, dropout=0.0):
 
 
         self.regularizer = regularizations.validate_regularizer(regularizer)
@@ -43,7 +43,6 @@ class Layer:
 
         if self.dropout==1:self.mask=np.zeros_like(self.currentOutput)
         else:self.mask= self.mask/(1-self.dropout)
-
         #self.mask = self.mask / (1 - self.dropout if self.dropout != 1 else 1)
         return self.currentOutput
 

NN_lib/regularizations.py
@@ -17,18 +17,31 @@ class Regularization:
         self.f = f
         self.dxf = dxf
 
-def l2regul(W, rlambda=0.0):
-    return (rlambda*(W**2)).sum()
+    def __str__(self):
+        return str(self.f.__name__)
 
-def l1regul(W, rlambda=0.0):
-    return (W*rlambda).sum()
+    def __repr__(self):
+        return str(self.f.__name__)
+
+def l2(W, rlambda=0.0):
+    return rlambda*np.sum((np.square(W)))
+
+def l1(W, rlambda=0.0):
+    return (np.abs(W)).sum()*rlambda
+
+def elasticNet(W,rlambda):
+    return l1(W,rlambda[0])+l2(W,rlambda[1])
+
+def elasticNetdx(W, rlambda):
+    return l1reguldx(W, rlambda[0]) + l2reguldx(W,rlambda[1])
 
 def l1reguldx(W, rlambda=0.0):
     return rlambda*(np.sign(W))
 
 def l2reguldx(W, rlambda=0.0):
-    return W*rlambda*2
+    return 2*(W*rlambda)
 
 reguls = dict()
-reguls["L2"] = Regularization(l2regul, l2reguldx)
-reguls["L1"] = Regularization(l1regul, l1reguldx)
+reguls["L2"] = Regularization(l2, l2reguldx)
+reguls["L1"] = Regularization(l1, l1reguldx)
+reguls["EN"] = Regularization(elasticNet, elasticNetdx)

NN_lib/validation.py
@@ -138,14 +138,15 @@ def grid_search(dataset, epochs, n_layers, neurons, activations=None,
                 result_grid[k] = r  # If no validation (val_split=0) then select best based on tr loss
             else:
                 result_grid[k] = v
+            var = 0
         else:
 
-            result_grid[k], _, _, _, history = k_fold_validation(dataset, net, epochs=params['epochs'], \
+            result_grid[k], var, _, _, _, history = k_fold_validation(dataset, net, epochs=params['epochs'], \
                                                                  optimizer=params['optimizers'], cvfolds=cvfolds,
                                                                  batch_size=params['batch_size'],val_loss_fun=val_loss_fun,
                                                                  loss_func=params['loss_fun'], verbose=verbose - 0)
         if (validating):
-            full_grid.append({'configuration': params, 'val_loss': result_grid[k], 'history': history})
+            full_grid.append({'configuration': params, 'val_loss': result_grid[k], 'in-fold var':var,'history': history})
         else:  # If no validation was done only put config and other stuff(to add..)
             full_grid.append({'configuration': params, 'history': history})
 
@@ -219,6 +220,6 @@ def k_fold_validation(dataset, NN, epochs, optimizer, cvfolds=3, batch_size=32, 
         for z in range(0, len(r[k])):
             r[k][z] = r[k][z] / cvfolds
 
-    return np.average(val_loss), np.average(val_acc), np.average(tr_loss), np.average(tr_acc), r
+    return np.average(val_loss), np.var(val_loss), np.average(val_acc), np.average(tr_loss), np.average(tr_acc), r
 # r is the average for each epoch
 # the other are the total average of the end