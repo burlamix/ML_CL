Test functions fixes and contour navigation plots. Conjugate gradient fixes

LSS.py
@@ -1,45 +1,93 @@
-
 from NN_lib import optimizers
 import numpy as np
 from NN_lib import linesearches
-def matyas_fun(W, only_fp=False):
-	
-    r = 0.26 * (W[0][0]**2 + W[0][1]**2)  -0.48*W[0][0]*W[0][1]; 
+from test_functions import rosenbrock, matyas_fun
+import pylab
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+from matplotlib.mlab import bivariate_normal
 
-    if only_fp:
-        return r
-    else:
-        return r,  np.array(np.array([[ 0.52*W[0][0]-0.48*W[0][1]  ,  0.52*W[0][1]-0.48*W[0][0] ] ]))
+def optimize_fun(fun,start,opt,epochs=100):
+    W = start
+    x = [W[0][0]]
+    y = [W[0][1]]
+    z = [fun(np.array([[x[0], y[0]]]), only_fp=True)]
 
-def g(W, only_fp=False):
-    r = 100*((W[1]-W[0]**2)**2)+(W[0]-1)**2
+    for i in range(0, epochs):
+        W = opt.optimize(fun, W)
+        x.append(W[0][0])
+        y.append(W[0][1])
+        newv = fun(W, only_fp=True)
+        z.append(newv)
+        print('final', newv)
 
-    if only_fp:
-        return r
-    else:
-        return r,  np.array([2*(200*W[0]**3 -200*W[0]*W[1]+W[0]-1), 200*(W[1]-W[0]**2)])
+    return {'x':np.array(x),'y':np.array(y),'z':np.array(z),'opt':opt}
 
+def navigate_fun(navs, plot):
+    #plt.switch_backend('QT4Agg')
+    #figManager = plt.get_current_fig_manager()
+    #figManager.window.showMaximized()
+    figManager = plt.get_current_fig_manager()
+    figManager.full_screen_toggle()
+    plt.ion()
+    cols = [np.random.rand(3,1) for i in range(0,len(navs))]
+    for i in range(0, len(navs[0]['x']) - 1):
+        for j in range(0,len(navs)):
+            px = [navs[j]['x'][i], navs[j]['x'][i + 1]]
+            py = [navs[j]['y'][i], navs[j]['y'][i + 1]]
+            pz = [navs[j]['z'][i], navs[j]['z'][i + 1]]
+            plot.plot(px, py, 'k', alpha=0.8, linewidth=1.5,
+                    c=cols[j],label=navs[j]['opt'].pprint())
+            if (i==0): plt.legend()
+        plt.pause(.001)
+    plt.pause(2**31)
+    #input("..")
 
-amg = linesearches.armj_wolfe(m1=1e-4, m2=0.9, lr=0.00001, min_lr=1e-11, scale_r=0.000001, max_iter=1000)
+def plot_contours(fun, xrange, yrange, contours=200):
+    fig = plt.figure()
+    # ax = fig.gca(projection='3d')
+    ax = fig.gca()
+    X, Y = np.meshgrid(xrange, yrange)
+    zs = np.array([fun([[xrange, yrange]], only_fp=True)
+                   for xrange, yrange in zip(np.ravel(X), np.ravel(Y))])
+    Z = zs.reshape(X.shape)
+    # surf=ax.plot_surface(Y,X,Z, cmap=cm.jet,cstride=1,rstride=1,alpha=0.3)
+    # surf = ax.contour(X, Y, Z, 10, lw=3, cmap="autumn_r", linestyles="solid", offset=-1)
+    surf = ax.contour(X, Y, Z, contours, lw=3, colors="k", linestyles="solid")
+    fig.colorbar(surf, shrink=0.5, aspect=5)
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    # ax.set_zlabel('Z Label')
+    ax.invert_xaxis()
+    return ax
 
+amg = linesearches.armj_wolfe(m1=1e-4, m2=0.9, lr=0.001, min_lr=1e-11, scale_r=0.5, max_iter=100)
+bt = linesearches.back_track(lr=0.15, m1=1e-4, scale_r=0.95, min_lr=1e-11, max_iter=200)
 
 lso = optimizers.RMSProp(lr=0.0001)
 lso = optimizers.Momentum(lr=0.00001)
 lso = optimizers.Adine(lr=0.01, ms=0.9, mg=1.0001, e=1.0, ls = None)
 lso = optimizers.Momentum(lr=0.01,eps=0.6)
 lso = optimizers.Adam(lr=0.1)
-#lso = optimizers.SimpleOptimizer(lr=0.01,ls= amg)
-lso = optimizers.RMSProp(lr=0.1)
-lso = optimizers.SimpleOptimizer(lr=0.01,ls= amg)
+lso = optimizers.Momentum(lr=0.1,eps=0.6)
+lso = optimizers.SimpleOptimizer(lr=0.1,ls= None)
 
-initW = np.array([[12,34.2]])
+fun = matyas_fun
+a = np.arange(-4, 5, 0.1)
+b = np.arange(6, -3, -0.1)
+plot = plot_contours(fun, xrange=a, yrange=b, contours=200)
 
-for i in range(0,500):
-    newW = lso.optimize(matyas_fun, initW)
-   # lso.reset()
-    initW = newW
-    print('final',matyas_fun(initW,only_fp=True))
+epochs=300
+lso = optimizers.Adam(lr=0.076,ls=amg)
+o1 = optimize_fun(fun,np.array([[-3, 5]]),opt=lso, epochs=epochs)
+lso = optimizers.SimpleOptimizer(lr=0.076,ls=amg)
+o2 = optimize_fun(fun,np.array([[-3, 5]]),opt=lso, epochs=epochs)
+lso = optimizers.Adam(lr=0.076)
+#o3 = optimize_fun(fun,np.array([[-3, 5]]),opt=lso, epochs=epochs)
+lso = optimizers.Adamax(lr=0.076)
+#o4 = optimize_fun(fun,np.array([[-3, 5]]),opt=lso, epochs=epochs)
 
-print('final',matyas_fun(initW,only_fp=True))
-print('at',initW)
+navigate_fun([o1,o2], plot=plot)
 

NN_lib/linesearches.py
@@ -19,13 +19,15 @@ def bisection( f, W,grad,eps):
     return alpha
 
 #Prolly not needed..
-def us_norm(x):
-    if (x[0].shape == ()):
-        return -np.linalg.norm(x)
+def us_norm(grad):
+    if(len((grad.shape)) == 2 ):
+        return -np.linalg.norm(grad)
+    if (grad[0].shape == ()):
+        return -np.linalg.norm(grad)
     else:
         return -(np.linalg.norm(
             np.concatenate(
-                [x[i].reshape(x[i].shape[1] * x[i].shape[0], 1) for i in range(0, len(x))])
+                [grad[i].reshape(grad[i].shape[1] * grad[i].shape[0], 1) for i in range(0, len(grad))])
         ))
 
 def dir_der(grad, lastg):

NN_lib/optimizers.py
@@ -5,14 +5,6 @@ import sklearn as sk
 
 from NN_lib.linesearches import dir_der,us_norm
 
-def us_norm(x):
-    if (x[0].shape == ()):
-        return -np.linalg.norm(x)
-    else:
-        return -(np.linalg.norm(
-            np.concatenate(
-                [x[i].reshape(x[i].shape[1] * x[i].shape[0], 1) for i in range(0, len(x))])
-        ))
 
 class SimpleOptimizer:
     #SGD optimizer without momentum
@@ -31,7 +23,9 @@ class SimpleOptimizer:
         pass
 
     def pprint(self):
-        return "sgd,lr="+str(self.lr)
+        ls="None"
+        if self.ls!=None: ls = self.ls.__name__
+        return "SGD{lr:"+str(self.lr)+",ls:"+ls+"}"
 
     def getLr(self):
         return self.lr
@@ -75,7 +69,9 @@ class Momentum:
         self.last_g = None
 
     def pprint(self):
-        return "lr=" + str(self.lr)+",m="+str(self.eps)
+        nesterov=""
+        if self.nesterov: nesterov="(nesterov)"
+        return "Momentum"+nesterov+"{lr:" + str(self.lr)+",m:"+str(self.eps)+"}"
 
     def getLr(self):
         return self.lr
@@ -88,8 +84,8 @@ class Momentum:
         if self.nesterov:
             #If nesterov, "look ahead" first
             loss, grad = \
-                (f(self.eps*self.last_g+W) if (self.last_g != None) else f(W))
-            v = -self.lr*(grad)+self.eps*(self.last_g if (self.last_g != None) else 0)
+                (f(self.eps*self.last_g+W) if (not(self.last_g is None)) else f(W))
+            v = -self.lr*(grad)+self.eps*(self.last_g if (not(self.last_g is None)) else 0)
         else:
             loss, grad = f(W)
             v = self.eps*(self.last_g if (not(self.last_g is None)) else 0) - self.lr*(grad)
@@ -107,20 +103,22 @@ class Adam:
     def __repr__(self):
         return str('adam'+str(self.lr))
 
-    def __init__(self, lr=0.001, b1=0.9, b2=0.999, eps=1e-8):
+    def __init__(self, lr=0.001, b1=0.9, b2=0.999, eps=1e-8,ls=None):
         self.lr = lr
         self.b1 = b1
         self.b2 = b2
         self.eps = eps
         self.reset()
+        self.ls = ls
 
     def reset(self):
         self.m = [0]
         self.v = [0]
         self.t = 0
+        self.ls=None
 
     def pprint(self):
-        return "adam:" + str(self.lr)
+        return "Adam{lr:" + str(self.lr)+",b1:"+str(self.b1)+",b2:"+str(self.b2)+"}"
 
     def optimize(self, f, W):
         if not(isinstance(f, types.FunctionType)):
@@ -138,7 +136,14 @@ class Adam:
         vcap = self.v[self.t]/(np.subtract(1,np.power(self.b2,self.t)))
         for i in range(len(vcap)):
             vcap[i] = np.sqrt(vcap[i])
-        return np.subtract(W,self.lr*mcap/((vcap+self.eps)))
+
+        if self.ls!=None:
+            loss,grad = f(W-self.lr*mcap/((vcap+self.eps)))#00118553246015
+            actual_lr = self.ls(f, W-self.lr*mcap/((vcap+self.eps)),loss, grad)
+        else:
+            actual_lr = self.lr
+
+        return np.subtract(W,actual_lr*mcap/((vcap+self.eps)))
 
 
 class Adamax:
@@ -164,7 +169,7 @@ class Adamax:
         self.t = 0
 
     def pprint(self):
-        return "adamax:" + str(self.lr)
+        return "Adamax{lr:" + str(self.lr)+",b1:"+str(self.b1)+",b2:"+str(self.b2)+"}"
 
     def optimize(self, f, W):
         if not(isinstance(f, types.FunctionType)):
@@ -200,7 +205,7 @@ class RMSProp:
         self.R = None
 
     def pprint(self):
-        return "rms:" + str(self.lr)
+        return "RMSprop{lr:" + str(self.lr)+",d:"+str(self.delta)+"}"
 
     def optimize(self, f, W):
         if not(isinstance(f, types.FunctionType)):
@@ -229,8 +234,12 @@ class ConjugateGradient:
     def reset(self):
         self.p = None
         self.last_g = None
+
     def pprint(self):
-        return "lr=" + str(self.lr)
+        ls="None"
+        if self.ls!=None: ls = self.ls.__name__
+        return "ConjugateGrad(Fletcher-Reeves){lr:" + str(self.lr)+\
+        ",eps:"+str(self.eps)+",ls:"+ls+"}"
 
     def getLr(self):
         return self.lr
@@ -244,7 +253,7 @@ class ConjugateGradient:
         loss, grad = f(W)
         #if np.random.random()<0.01:self.p=None
 
-        if self.p == None:
+        if self.p is None:
             self.p = -grad#/us_norm(grad)
         else:
             beta = (-us_norm(grad)/(-us_norm(self.last_g)))**2
@@ -252,9 +261,9 @@ class ConjugateGradient:
             #print(beta)
             self.p = -grad + beta*self.p
         self.last_g = grad
-        if self.ls!=None and 1==1:
-
-            actual_lr = self.ls(f, W+self.lr*self.p,f(W+self.lr*self.p,only_fp=True), -self.p)
+        if self.ls!=None:
+            loss,grad= f(W+self.lr*self.p)
+            actual_lr = self.ls(f, W+self.lr*self.p,loss, grad)
         else:
             actual_lr = self.lr
         return (W+actual_lr*self.p)
@@ -283,7 +292,7 @@ class Adine:
         self.avgl = 0
 
     def pprint(self):
-        return "lr=" + str(self.lr)+",m="+str(self.eps)
+        return "Adine{lr:" + str(self.lr)+",ms:"+str(self.ms)+",mg:"+str(self.mg)+",t:"+self.e+"}"
 
     def getLr(self):
         return self.lr