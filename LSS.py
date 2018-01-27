Conjugate gradient fixes (---seems--- fine now). Contour plots documentation

LSS.py
@@ -9,7 +9,24 @@ from matplotlib import cm
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
 from matplotlib.mlab import bivariate_normal
 
+
 def optimize_fun(fun,start,opt,epochs=100):
+    '''
+    Optimizes the given function with the given parameters and returns the history
+    of iterations.
+    :param fun: The function to optimizes: must be of the form f(W, only_fp)
+    where W is the 2-arguments input matrix (e.g. np.array([[1,2]])) denoting
+    the point to evaluate the function at and only_fp is a parameter that
+    determines whether we want both the function value and the gradient at W
+    or only the value. f must return the value only if only_fp is set to True
+    and both the value and gradient (value,gradient) if only_fp is set to False.
+    :param start: Denoattes the starting point to begin optimizing the function from
+    :param opt: An object denoting the optimizer to utilize for the optimization of f.
+    :param epochs: The number of epochs to optimize f over.
+    :return: A dictionary of entries 'x', 'y', 'z', 'opt' denoting respectively
+    the history of x coordinates, y coordinates, the corresponding function value
+    over the iterations and the input optimizer object.
+    '''
     W = start
     x = [W[0][0]]
     y = [W[0][1]]
@@ -21,11 +38,50 @@ def optimize_fun(fun,start,opt,epochs=100):
         y.append(W[0][1])
         newv = fun(W, only_fp=True)
         z.append(newv)
-        print('final', newv)
+        print('Function value:', newv)
 
     return {'x':np.array(x),'y':np.array(y),'z':np.array(z),'opt':opt}
 
+
+def plot_contours(fun, xrange, yrange, contours=200):
+    '''
+    Plots the specified number of contours of the given input fun, over the given range.
+    :param fun: A function of the format specified in @optimize_fun
+    :param xrange: The range of x values to plot the contours over. Note that length
+    of xrange and yrange must match.
+    :param yrange: The range of y values to plot the contours over. Note that length
+    of xrange and yrange must match.
+    :param contours: The number of contours to display
+    :return: The resulting plot handler (use it it @navigate_fun
+    '''
+    fig = plt.figure()
+    # ax = fig.gca(projection='3d')optimize_fun
+    ax = fig.gca()
+    X, Y = np.meshgrid(xrange, yrange)
+    zs = np.array([fun([[xrange, yrange]], only_fp=True)
+                   for xrange, yrange in zip(np.ravel(X), np.ravel(Y))])
+    Z = zs.reshape(X.shape)
+    # surf=ax.plot_surface(Y,X,Z, cmap=cm.jet,cstride=1,rstride=1,alpha=0.3)
+    # surf = ax.contour(X, Y, Z, 10, lw=3, cmap="autumn_r", linestyles="solid", offset=-1)
+    surf = ax.contour(X, Y, Z, contours, lw=3, colors="k", linestyles="solid")
+    fig.colorbar(surf, shrink=0.5, aspect=5)
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    # ax.set_zlabel('Z Label')
+    ax.invert_xaxis()
+    return ax
+
+
 def navigate_fun(navs, plot):
+    '''
+    Given an array of histories of evaluations (see @optimize_fun return value) and
+    an appropriate contours plot (see @plot_contours) displays the interactive
+    navigation of the optimizer contained in navs epoch by epoch.
+    :param navs: An array of dictionaries formatted as specified in the return value
+    of @optimize_fun.
+    :param plot: Contour plot handler as specified in @plot_contours
+    '''
+
     #plt.switch_backend('QT4Agg')
     #figManager = plt.get_current_fig_manager()
     #figManager.window.showMaximized()
@@ -45,26 +101,9 @@ def navigate_fun(navs, plot):
     plt.pause(2**31)
     #input("..")
 
-def plot_contours(fun, xrange, yrange, contours=200):
-    fig = plt.figure()
-    # ax = fig.gca(projection='3d')
-    ax = fig.gca()
-    X, Y = np.meshgrid(xrange, yrange)
-    zs = np.array([fun([[xrange, yrange]], only_fp=True)
-                   for xrange, yrange in zip(np.ravel(X), np.ravel(Y))])
-    Z = zs.reshape(X.shape)
-    # surf=ax.plot_surface(Y,X,Z, cmap=cm.jet,cstride=1,rstride=1,alpha=0.3)
-    # surf = ax.contour(X, Y, Z, 10, lw=3, cmap="autumn_r", linestyles="solid", offset=-1)
-    surf = ax.contour(X, Y, Z, contours, lw=3, colors="k", linestyles="solid")
-    fig.colorbar(surf, shrink=0.5, aspect=5)
-    ax.set_xlabel('X')
-    ax.set_ylabel('Y')
-    # ax.set_zlabel('Z Label')
-    ax.invert_xaxis()
-    return ax
 
-amg = linesearches.armj_wolfe(m1=1e-4, m2=0.9, lr=0.001, min_lr=1e-11, scale_r=0.5, max_iter=100)
-bt = linesearches.back_track(lr=0.15, m1=1e-4, scale_r=0.95, min_lr=1e-11, max_iter=200)
+amg = linesearches.armj_wolfe(m1=1e-4, m2=0.9, lr=0.2, min_lr=1e-11, scale_r=0.9, max_iter=200)
+bt = linesearches.back_track(lr=1, m1=1e-4, scale_r=0.9, min_lr=1e-11, max_iter=200)
 
 lso = optimizers.RMSProp(lr=0.0001)
 lso = optimizers.Momentum(lr=0.00001)
@@ -79,10 +118,10 @@ a = np.arange(-4, 5, 0.1)
 b = np.arange(6, -3, -0.1)
 plot = plot_contours(fun, xrange=a, yrange=b, contours=200)
 
-epochs=300
-lso = optimizers.Adam(lr=0.076,ls=amg)
+epochs=500
+lso = optimizers.ConjugateGradient(lr=0.076,ls=amg)
 o1 = optimize_fun(fun,np.array([[-3, 5]]),opt=lso, epochs=epochs)
-lso = optimizers.SimpleOptimizer(lr=0.076,ls=amg)
+lso = optimizers.ConjugateGradient(lr=0.076,ls=bt)
 o2 = optimize_fun(fun,np.array([[-3, 5]]),opt=lso, epochs=epochs)
 lso = optimizers.Adam(lr=0.076)
 #o3 = optimize_fun(fun,np.array([[-3, 5]]),opt=lso, epochs=epochs)

NN_lib/optimizers.py
@@ -229,6 +229,7 @@ class ConjugateGradient:
         self.ls = ls
         self.p = None
         self.last_g = None
+        self.t = 0
         self.reset()
 
     def reset(self):
@@ -248,24 +249,33 @@ class ConjugateGradient:
 
         if not(isinstance(f, types.FunctionType)):
             sys.exit("Provided function is invalid")
-
+        self.t+=1
 
         loss, grad = f(W)
+
         #if np.random.random()<0.01:self.p=None
 
         if self.p is None:
-            self.p = -grad#/us_norm(grad)
+            self.p = -grad#/(-us_norm(grad))
         else:
-            beta = (-us_norm(grad)/(-us_norm(self.last_g)))**2
+            beta = -us_norm(grad)#/(-us_norm(self.last_g)))
+            #beta = -grad/(-us_norm(grad))
             #beta = dir_der(grad,grad)/dir_der(self.last_g,self.last_g)
             #print(beta)
             self.p = -grad + beta*self.p
         self.last_g = grad
         if self.ls!=None:
             loss,grad= f(W+self.lr*self.p)
-            actual_lr = self.ls(f, W+self.lr*self.p,loss, grad)
+            if np.abs(us_norm(grad))>1e-9:
+                actual_lr = self.ls(f, W+self.lr*self.p,loss, grad)
+            else: actual_lr = self.lr
+            #print('actual',actual_lr)
         else:
             actual_lr = self.lr
+
+        f2 = f(W + actual_lr * (self.p),only_fp=True)
+        if (np.abs(f2)>np.abs(loss)):
+            self.p = -grad/(-us_norm(grad))
         return (W+actual_lr*self.p)
 
 class Adine:

to_line.py
@@ -11,10 +11,10 @@ from keras.layers import Dense
 from keras import regularizers
 from keras import optimizers as optims
 outs=2
-valr=(0,3.8)
+valr=(0,0)
 clr=0.01
 drop=0.0
-epochs=10
+epochs=10000
 inps=10
 #13770039132
 np.random.seed(23185)
@@ -33,12 +33,15 @@ dataset.init_test(preproc.load_data(path=test_data_path, target=False, header_l=
 
 #print(np.linalg.cond(np.eye((10))+np.dot(dataset.train[0].T,dataset.train[0])))
 amg = linesearches.armj_wolfe(m1=1e-4, m2=0.9, lr=clr, min_lr=1e-11, scale_r=0.99, max_iter=1000)
+amg = linesearches.armj_wolfe(m1=1e-4, m2=0.9, lr=0.1, min_lr=1e-11, scale_r=0.9, max_iter=100)
+
 #amg = linesearches.back_track(lr=1, m1=1e-4, scale_r=0.1, min_lr=1e-11, max_iter=100)
 
 #optimizer = ConjugateGradient( lr=clr, ls=amg)
 optimizer = SimpleOptimizer( lr=clr, ls=amg)
 optimizer = Adine( lr=clr, ls=None)
 optimizer = SimpleOptimizer( lr=clr, ls=amg)
+optimizer = ConjugateGradient( lr=clr, ls=amg)
 #optimizer = Momentum( lr=clr)
 #optimizer = Adam(lr=0.01 )
 
@@ -54,9 +57,9 @@ optimizer = SimpleOptimizer( lr=clr, ls=amg)
 #    NN.fit_ds( dataset,epochs, optimizer ,batch_size=dataset.train[0].shape[0],verbose=2,loss_func="mse")
 
 NN = NeuralNetwork()
-#NN.addLayer(inputs=inps,neurons=25,activation="tanh", rlambda=valr,regularization="EN",
-#            dropout=0,bias=0.0)
-NN.addLayer(inputs=inps,neurons=outs,activation="linear",rlambda=valr,regularization="EN",bias=0.0)
+NN.addLayer(inputs=inps,neurons=25,activation="tanh", rlambda=valr,regularization="EN",
+            dropout=0,bias=0.0)
+NN.addLayer(inputs=25,neurons=outs,activation="linear",rlambda=valr,regularization="EN",bias=0.0)
 #NN.set_weights(weights)
 (loss, acc, val_loss, val_acc, history2)=\
     NN.fit_ds( dataset,epochs, optimizer  ,val_split=0,batch_size=dataset.train[0].shape[0],verbose=2,loss_func="mse")