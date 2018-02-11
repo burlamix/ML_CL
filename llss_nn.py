fab386b6ee9aad9dc7ff7fd6803c3407a63c6525
burlamix <burlamix@gmail.com>
2018-02-11 12:13:05 +0100

    read me, and chanche to lls_nn

--- llss_nn.py
+++ llss_nn.py
@@ -30,7 +30,7 @@ dataset = preproc.Dataset()
 train_data_path = "data/myTrain.csv"
 test_data_path = "data/myTest.csv"
 dataset.init_train(preproc.load_data(path=train_data_path, target=True, header_l=0, targets=2))
-dataset.init_test(preproc.load_data(path=test_data_path, target=True, header_l=0))
+dataset.init_test(preproc.load_data(path=test_data_path, target=True, header_l=0, targets=2))
 
 preprocessor = preproc.Preprocessor()
 preprocessor.shuffle(dataset)
@@ -59,10 +59,24 @@ numpy_sol = np.linalg.lstsq(biasedX,dataset.train[1]) #Get the optimal weights
 numpy_pred = np.dot(biasedX,numpy_sol[0]) #Predict the output with the optimal weights
 numpy_loss = loss_functions.mse(dataset.train[1],numpy_pred) #Calculate the loss
 
-_, _, normeqs_loss = normeqs_solve(biasedX, dataset.train[1])
-_, _, qr_loss = QR_solve(biasedX, dataset.train[1])
+normeqs_sol, _, normeqs_loss = normeqs_solve(biasedX, dataset.train[1])
+qr_sol, _, qr_loss = QR_solve(biasedX, dataset.train[1])
 
 print('Neural network error:',loss)
 print('Numpy linear least squares solver error:',numpy_loss)
 print('Normal equations error:',normeqs_loss)
 print('QR solver error:',qr_loss)
+
+######## GENERALIZATION ERROR WITH LLSSs ############
+#Prepare test data for the linear least square solver - include the bias
+biasedXtest = dataset.test[0]
+biasedXtest = np.concatenate((np.ones((biasedXtest.shape[0],1)),biasedXtest),axis=1)
+
+qr_pred = np.dot(biasedXtest, qr_sol)  # Predict the output with the optimal weights
+qr_loss = loss_functions.mse(dataset.test[1], qr_pred)  # Calculate the loss
+normeqs_pred = np.dot(biasedXtest, normeqs_sol)  # Predict the output with the optimal weights
+normeqs_loss = loss_functions.mse(dataset.test[1], qr_pred)  # Calculate the loss
+
+print("\n*** GENERALIZATION ERROR ***\n")
+print('QR solver error on test set:',qr_loss)
+print('Normal equations error on test set:',normeqs_loss)
